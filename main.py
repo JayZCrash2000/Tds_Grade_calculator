97d597adeb6a94aa1accfc63eb94aedafa3de4d9
selvaiitm <96175511+selvaiitm@users.noreply.github.com>
2023-12-28 18:43:16 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,20 @@
 import streamlit as st
 
-st.text('welcome to this app for calculating the final grade for tds')
+st.text('')
+
+
+st.markdown("<hr>", unsafe_allow_html=True)
+st.markdown("""
+<h1 align="center">  TDS Grade Calculator </h1>
+""", unsafe_allow_html=True)
+
+st.markdown("<hr>", unsafe_allow_html=True)
+
+st.markdown("""
+<h3 align="center"> Welcome this app is created  for calculating the final grade for TDS of september-2023 term, a Diploma level course for  BS IN DATA SCIENCE COURSE OFFERED BY IIT MADRAS 
+""", unsafe_allow_html=True)
+
+
+
+st.markdown("<hr>", unsafe_allow_html=True)
+
